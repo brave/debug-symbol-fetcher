9caf4e695cca96129e93561fa4836f3605f28324
Matt Bacchi <mbacchi@gmail.com>
2018-07-30 16:32:23 -0600

    Use brave-browser release url; download linux symbols

--- fetch.sh
+++ fetch.sh
@@ -2,10 +2,10 @@
 set -eux
 
 MUON_URL="https://github.com/brave/muon/releases/download"
-CORE_URL="https://github.com/brave/brave-browser-builds/releases/download"
+CORE_URL="https://github.com/brave/brave-browser/releases/download"
 
 MUON_VERSIONS="${CORE_VERSIONS:-$(curl -s https://api.github.com/repos/brave/muon/releases| jq .[].tag_name| sed 's/^"v//g; s/"$//g')}"
-CORE_VERSIONS="${CORE_VERSIONS:-$(curl -s https://api.github.com/repos/brave/brave-browser-builds/releases| jq .[].tag_name| sed 's/^"v//g; s/"$//g')}"
+CORE_VERSIONS="${CORE_VERSIONS:-$(curl -s https://api.github.com/repos/brave/brave-browser/releases| jq .[].tag_name| sed 's/^"v//g; s/"$//g')}"
 
 FORCE_UNZIP="${FORCE_UNZIP:-}"
 
@@ -35,9 +35,11 @@ do
         cmd="bsdtar -xvf-"
         test -f "symbols/win32-x64-$VERSION/LICENSE" || ( cd "symbols/win32-x64-$VERSION" ; wget -O - "$CORE_URL/v${VERSION}/brave-v${VERSION}-win32-x64-symbols.zip" | $cmd )
         test -f "symbols/darwin-$VERSION/LICENSE" || ( cd "symbols/darwin-$VERSION" ; wget -O - "$CORE_URL/v${VERSION}/brave-v${VERSION}-darwin-x64-symbols.zip" | $cmd )
+        test -f "symbols/linux-$VERSION/LICENSE" || ( cd "symbols/linux-$VERSION" ; wget -O - "$CORE_URL/v${VERSION}/brave-v${VERSION}-linux-x64-symbols.zip" | $cmd )
     elif [ "$(which unzip)" ]; then
         cmd="unzip -x"
         test -f "symbols/win32-x64-$VERSION/LICENSE" || ( cd "symbols/win32-x64-$VERSION" ; wget -O - "$CORE_URL/v${VERSION}/brave-v${VERSION}-win32-x64-symbols.zip" > /tmp/win32-x64.zip ; $cmd /tmp/win32-x64.zip ; rm /tmp/win32-x64.zip)
         test -f "symbols/darwin-$VERSION/LICENSE" || ( cd "symbols/darwin-$VERSION" ; wget -O - "$CORE_URL/v${VERSION}/brave-v${VERSION}-darwin-x64-symbols.zip" > /tmp/darwin.zip ; $cmd /tmp/darwin.zip ; rm /tmp/darwin.zip)
+        test -f "symbols/linux-$VERSION/LICENSE" || ( cd "symbols/linux-$VERSION" ; wget -O - "$CORE_URL/v${VERSION}/brave-v${VERSION}-linux-x64-symbols.zip" > /tmp/linux.zip ; $cmd /tmp/linux.zip ; rm /tmp/linux.zip)
     fi
 done
